30a177552b6a6f6c2471c4848001fd50e7080c3c
Work <Work@MacBook-Pro-5.local>
2025-03-19 11:49:26 -0400

    Remove test function

--- sql_utils/db_utils.py
+++ sql_utils/db_utils.py
@@ -85,8 +85,6 @@ def df_to_sql_table(df, table_name, schema='analytics_ny', conn=None):
         df_chunk.to_sql(table_name, conn, schema=schema, if_exists='append', index=False, method='multi')
 
 
-def test_func_2():
-    print('test')
 
 
 # Main function to run the script
