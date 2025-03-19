07cf714cdceef7143b2a5eae00e5a2a34a04ce5f
Work <Work@MacBook-Pro-5.local>
2025-03-19 10:40:53 -0400

    test

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sql_utils',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'sqlalchemy',
+        'trino'
+    ],
+    description='A collection of utilities for working with SQL databases',
+    author='Levi Leach',
+    author_email='levi.leach@rokt.com',
+    url='https://github.com/levileachrokt/sql-utils'
+    
+)
