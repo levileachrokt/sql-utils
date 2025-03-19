6307b29ae850d899db0d2c691590da4570d718cb
Work <Work@MacBook-Pro-5.local>
2025-03-19 11:27:11 -0400

    still testing

--- sql_utils/__init__.py
+++ sql_utils/__init__.py
@@ -1,3 +1,3 @@
-from .db_utils import run_sql, sql_to_csv, create_sql_connection, df_to_sql_table  # Import specific functions
+from .db_utils import run_sql, sql_to_csv, create_sql_connection, df_to_sql_table, test_func_2  # Import specific functions
     
 __all__ = ["run_sql", "sql_to_csv", "create_sql_connection", "df_to_sql_table", "test_func_2"]
